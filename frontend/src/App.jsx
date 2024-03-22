6d35c870f71338ed41748ed4b4418675ac8a1c4a
Alex Appleget <alexappleget2014@gmail.com>
2024-03-22 14:41:36 -0500

    Commit 4 - created backend and used nodemailer to set up a system to email the user when they enter their info into our contact form. We will just need to come up with a dummy email address for both of us to use.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -79,6 +79,23 @@ function Text() {
 }
 
 function Contact() {
+  //this is grabbing the value of the user's email they input
+  const [email, setEmail] = useState("");
+  //to help confirm the user the email was sent
+  const [emailSent, setEmailSent] = useState(false);
+
+  //function that sends the email to them
+  const handleEmail = async () => {
+    try {
+      await axios.post("http://localhost:5174/contact-us", {
+        recipientEmail: email, //remember recipientEmail on the backend? the value of 'email' will be what they input and be used as 'recipientEmail' on backend and be sent to it specifically
+      });
+      setEmailSent(true); //when user clicks the button itll set 'emailSent' to true and render words
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="contact-content">
       <h1>Contact Us</h1>
@@ -86,11 +103,24 @@ function Contact() {
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" />
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" />
+        {/* will update the value of email based on what they input */}
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
         <label htmlFor="message">Message:</label>
         <textarea id="message" cols="30" rows="10"></textarea>
         <button type="submit">Contact Us</button>
       </form>
+      {/* what will be shown once the email gets sent */}
+      {emailSent && (
+        <p>
+          We sent you a confirmation email! Thank you for reaching out to us.
+        </p>
+      )}
     </div>
   );
 }
