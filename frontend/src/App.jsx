de058b12715448eb4d39b69e29ede034040dbde5
Alex Appleget <alexappleget2014@gmail.com>
2024-03-22 13:39:01 -0500

    Commit 3 - added buttons to be used as tabs for users to navigate to each. When buttons are clicked, it sets the value of active to whatever string it says. When active equals a certain string. The specific button pressed will toggle to a certain color and show specific content. That way users know what tab they are in. Also, did a simple css style to show that the changes work and things are rendering correctly.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,72 @@
+import { useState } from "react";
+
 export default function App() {
+  const [active, setActive] = useState("Home");
+
   return (
-    <>
-      <h1>Hey Arhman!</h1>
-    </>
+    <div className="app">
+      <button
+        className={active === "Home" ? "btn-active" : ""}
+        onClick={() => setActive("Home")}
+      >
+        Home
+      </button>
+      <button
+        className={active === "Frontend" ? "btn-active" : ""}
+        onClick={() => setActive("Frontend")}
+      >
+        Frontend Dev
+      </button>
+      <button
+        className={active === "Backend" ? "btn-active" : ""}
+        onClick={() => setActive("Backend")}
+      >
+        Backend Dev
+      </button>
+      <button
+        className={active === "Text" ? "btn-active" : ""}
+        onClick={() => setActive("Text")}
+      >
+        Text
+      </button>
+      <div className="fields">
+        {active === "Home" && <Home />}
+        {active === "Frontend" && <Frontend />}
+        {active === "Backend" && <Backend />}
+        {active === "Text" && <Text />}
+      </div>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="home-content">
+      <h1>This is Home!</h1>
+    </div>
+  );
+}
+
+function Frontend() {
+  return (
+    <div className="frontend-content">
+      <h1>This is Frontend!</h1>
+    </div>
+  );
+}
+
+function Backend() {
+  return (
+    <div className="backend-content">
+      <h1>This is Backend!</h1>
+    </div>
+  );
+}
+
+function Text() {
+  return (
+    <div className="text-content">
+      <h1>This is Text!</h1>
+    </div>
   );
 }
