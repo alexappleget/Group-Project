4264de88c47a241d04bd59df7b1c3b47b7f43c04
Alex Appleget <alexappleget2014@gmail.com>
2024-03-26 10:19:30 -0500

    added a test connection to the code. when we run the backend for the userdata.js, now it tells us that we have successfully connected to the database.

--- backend/dataconnect.js
+++ backend/dataconnect.js
@@ -1,12 +1,29 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  user: "postgres",
-  password: process.env.SQL_PASS,
-  host: "localhost",
-  port: 5432,
-  database: "userinfo",
-});
+function createPool() {
+  const { SQL_USER, SQL_PASS, SQL_HOST, SQL_PORT, SQL_DATABASE } = process.env;
+  if (!SQL_USER || !SQL_PASS || !SQL_HOST || !SQL_PORT || !SQL_DATABASE) {
+    throw new Error("Incomplete database configuration.");
+  }
 
-module.exports = pool;
+  const pool = new Pool({
+    user: SQL_USER,
+    password: SQL_PASS,
+    host: SQL_HOST,
+    port: SQL_PORT,
+    database: SQL_DATABASE,
+  });
+
+  //to test the connection to the database
+  pool.query("SELECT NOW()", (err, res) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+    } else {
+      console.log("Successfully connected to the database:", res.rows[0]);
+    }
+  });
+
+  return pool;
+}
+module.exports = createPool();
