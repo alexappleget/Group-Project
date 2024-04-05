3437fafce7ab528d6091ba39d750da13dacf79a8
Alex Appleget <alexappleget2014@gmail.com>
2024-04-05 16:22:39 -0500

    added another email backend to use for the example to show what backend can do. then made and edited the 'Where' section

---
+++ backend/testemail.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const nodeMailer = require("nodemailer");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json());
+
+const { EMAIL_HOST, EMAIL_USER, EMAIL_PASS, EMAIL_PORT } = process.env;
+
+const transporter = nodeMailer.createTransport({
+  host: EMAIL_HOST,
+  port: EMAIL_PORT,
+  secure: true,
+  auth: {
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
+  },
+});
+
+app.post("/test-email", async (req, res) => {
+  //this will be used in the frontend to be able to take whatever email they input to send the email to them
+  const { recipientEmail } = req.body;
+
+  //this is what the email will look like
+  const html = `
+  <h3>Look at that!</h3>
+  <br />
+  <p>Isn't it cool that you can do something like this as a programmer?</p>
+  <br />
+  <p>Now that you have a little taste of what frontend, backend, and fullstack are, which one do you like most? Once you've decided, head back to the website and the tabs, in the top right, to learn about them more in-depth. See more examples and how to get started!</p>
+  `;
+
+  //what the user will see in their inbox
+  try {
+    const info = await transporter.sendMail({
+      from: "Test <noreplytravelprojectemail@gmail.com",
+      to: recipientEmail,
+      subject: "Hey!",
+      html: html,
+    });
+
+    console.log("Message sent:" + info.messageId);
+    res.status(200).send("Email sent successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error sending email");
+  }
+});
+
+//to let us know that the backend is working and listening for a response on the frontend
+app.listen(5176, () => {
+  console.log("Server is running on port 5176");
+});
---
+++ frontend/src/Components/Where.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "../Stylesheets/Where.css";
+import axios from "axios";
+
+function Where() {
+  const [clicked, setClicked] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleEmail = async () => {
+    try {
+      await axios.post("http://localhost:5176/test-email", {
+        recipientEmail: email,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="where-content">
+      <h1>Where Can I Start?</h1>
+      <p>
+        Before we dive into that, let me explain to you the different types of
+        programming. It's best to learn what each one does, and then decide from
+        there. That way you know what you are getting into.
+      </p>
+      <div className="whereFrontend-content">
+        <h3>Frontend</h3>
+        <p>
+          The frontend is what your users see and includes visual elements like
+          buttons, checkboxes, graphics, and text messages. It allows your users
+          to interact with your application. Some other examples are this entire
+          website. Everything you interact with to navigate around and click, is
+          all frontend. Go ahead and check out the button below to get a little
+          taste of what frontend can do!
+        </p>
+        <button className="clickExample-btn" onClick={() => setClicked(true)}>
+          Click Me!
+        </button>
+        {clicked && (
+          <div className="example-div">
+            <p>
+              Look what frontend can do! With just a click of a button, you can
+              create a pop-up window right here on the same page. Now go ahead
+              and click the button below to close the window and then move on to
+              learn about backend. There aren't any examples to show because
+              backend isn't anything visual to be seen. In order to visually see
+              examples of backend, that will tie into fullstack. Go ahead and
+              learn about full stack below!
+            </p>
+            <button onClick={() => setClicked(false)}>Close</button>
+          </div>
+        )}
+      </div>
+      <div className="whereBackend-content">
+        <h3>Backend</h3>
+        <p>
+          The backend is the data and infrastructure that make your application
+          work. It stores and processes application data for your users. Some
+          examples are if you want to store customer information, send
+          confirmation emails to clients, or anything that you think goes on
+          "behind the scenes".
+        </p>
+      </div>
+      <div className="whereFullStack-content">
+        <h3>Full Stack</h3>
+        <p>
+          Full Stack is a mix of both the frontend and the backend. Try out the
+          example below by inputing your email and hitting 'Send'. Then you will
+          receive an email. This is full stack! You build the backend part to
+          send an email and then build the frontend. The frontend provides the
+          input field, for you to enter in your email, and then the button to
+          click that will send the email. Go ahead and give it a try! Input your
+          email and click send!
+        </p>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button onClick={handleEmail}>Send Email</button>
+      </div>
+    </div>
+  );
+}
+
+export default Where;
