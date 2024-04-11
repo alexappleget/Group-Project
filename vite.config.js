2e777a2c0a9bd6a13618902fd338da26f8fb444e
Alex Appleget <alexappleget2014@gmail.com>
2024-04-11 17:15:50 -0500

    installed babel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import babel from "@rollup/plugin-babel";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**", // exclude node_modules
+    }),
+  ],
+});
