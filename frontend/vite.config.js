c53f60be03c9c51e9373f6a51304acb8d50d33ea
Alex Appleget <alexappleget2014@gmail.com>
2024-04-12 15:54:26 -0500

    made adjustments. changed root to index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "./src/main.jsx",
+  root: "index.html",
   build: {
-    outDir: "./public",
+    outDir: "/public",
   },
 });
