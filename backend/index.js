5a01f812e5d6f08ed0ff440a6c270b4bfe621563
Alex Appleget <alexappleget2014@gmail.com>
2024-04-13 15:50:19 -0500

    asgeawt

--- backend/index.js
+++ backend/index.js
@@ -117,7 +117,7 @@ pool.query("SELECT NOW()", (err, res) => {
   }
 });
 
-module.exports = app;
+module.exports = pool;
 
 // Code that will take whatever a user inputs into the contact form, and send it to our database to look at
 app.post("/users", async (req, res) => {
