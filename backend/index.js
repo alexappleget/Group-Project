1b358e05126fe3d76031162d29161a692e8beb6e
Alex Appleget <alexappleget2014@gmail.com>
2024-04-13 20:32:52 -0500

    moved app post code inside createPool function

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,6 @@ require("dotenv").config();
 const app = express();
 const { Pool } = require("pg");
 
-const pool = createPool();
-
 const { FRONTEND_URL } = process.env;
 
 app.use(
@@ -111,6 +109,34 @@ function createPool() {
     },
   });
 
+  // Code that will take whatever a user inputs into the contact form, and send it to our database to look at
+  app.post("/users", async (req, res) => {
+    try {
+      const { name, email, message } = req.body;
+      const newUser = await pool.query(
+        "INSERT INTO userdata (name, email, message) VALUES ($1, $2, $3)",
+        [name, email, message]
+      );
+      res.json(newUser.rows[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // Code used to for the registering example. It'll take the inputted values to save into the table
+  app.post("/testregister", async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const newRegister = await pool.query(
+        "INSERT INTO testdata (username, password) VALUES ($1, $2)",
+        [username, password]
+      );
+      res.json(newRegister.rows[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
   //to test the connection to the database
   pool.query("SELECT NOW()", (err, res) => {
     if (err) {
@@ -120,79 +146,55 @@ function createPool() {
     }
   });
 
+  // Code used to check the username and password in the database before logging in
+  app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      console.log("Received login request for username:", username);
+
+      const result = await pool.query(
+        "SELECT * FROM testdata WHERE username = $1",
+        [username]
+      );
+      console.log("Result from database query:", result.rows);
+
+      if (result.rows.length === 0) {
+        console.log("User not found");
+        return res
+          .status(401)
+          .json({ message: "Invalid username or password" });
+      }
+
+      const user = result.rows[0];
+
+      if (password !== user.password) {
+        console.log("Invalid password");
+        return res
+          .status(401)
+          .json({ message: "Invalid username or password" });
+      }
+
+      console.log("Login successful for user:", username);
+      return res.status(200).json({ message: "Login Successful" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+
+  // Code to delete the data from the data table
+  app.post("/delete", async (req, res) => {
+    try {
+      const deleteUser = await pool.query("DELETE FROM testdata");
+      res.json(deleteUser.rows);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
   return pool;
 }
 module.exports = createPool;
-
-// Code that will take whatever a user inputs into the contact form, and send it to our database to look at
-app.post("/users", async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
-    const newUser = await pool.query(
-      "INSERT INTO userdata (name, email, message) VALUES ($1, $2, $3)",
-      [name, email, message]
-    );
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-// Code used to for the registering example. It'll take the inputted values to save into the table
-app.post("/testregister", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const newRegister = await pool.query(
-      "INSERT INTO testdata (username, password) VALUES ($1, $2)",
-      [username, password]
-    );
-    res.json(newRegister.rows[0]);
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-// Code used to check the username and password in the database before logging in
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    console.log("Received login request for username:", username);
-
-    const result = await pool.query(
-      "SELECT * FROM testdata WHERE username = $1",
-      [username]
-    );
-    console.log("Result from database query:", result.rows);
-
-    if (result.rows.length === 0) {
-      console.log("User not found");
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-
-    const user = result.rows[0];
-
-    if (password !== user.password) {
-      console.log("Invalid password");
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-
-    console.log("Login successful for user:", username);
-    return res.status(200).json({ message: "Login Successful" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-// Code to delete the data from the data table
-app.post("/delete", async (req, res) => {
-  try {
-    const deleteUser = await pool.query("DELETE FROM testdata");
-    res.json(deleteUser.rows);
-  } catch (err) {
-    console.error(err);
-  }
-});
 
 app.listen(5175, () => {
   console.log("Server is running on port 5175");
