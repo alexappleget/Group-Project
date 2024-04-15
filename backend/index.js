4da60a4e7bc2f50c40ce3813246796a0b468ba4c
Alex Appleget <alexappleget2014@gmail.com>
2024-04-15 17:15:11 -0500

    cors update

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const app = express();
-app.options("*", cors());
+app.use(
+  cors({
+    origin: "https://dev-help-theta.vercel.app/",
+  })
+);
 app.use(express.json());
 
 // Database connection setup
